8ca002593b5bde859ba43a05dfd49c6312e4b64f
Kyra D. <kyra@existing.me>
2013-05-17 07:57:54 -0300

    Create feeds.php

---
+++ feeds.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Content-Type: application/xhtml+xml');
+
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<rss xmlns:syndacation="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/" version="2.0">
+  <channel>
+  	<title>.it</title>
+		<description></description>
+		<copyright></copyright>
+		<link></link>
+		<language></language>
+		
+		<item>
+			<title>Hello Feed 2!</title>
+			<link>http://google.com</link>
+			<description>test</description>
+			<pubDate>Wed, 16 May 2013 04:17:18 -0500</pubDate>
+		</item>
+    
+    
+		<item>
+			<title>Hello Feed!</title>
+			<link>http://google.com</link>
+			<description>test</description>
+			<pubDate>Wed, 17 May 2013 04:17:18 -0500</pubDate>
+		</item>
+    
+	</channel>
+</rss>
